b26a318b0bc72f632b6b64752169e720dae3fe5a
Younes Bourogaa <ybouroga@f4r1s1.clusters.42paris.fr>
2025-09-07 19:22:15 +0200

    correction du bug is on ecrit /

--- libft_ms/ft_files.c
+++ libft_ms/ft_files.c
@@ -6,7 +6,7 @@
 /*   By: ybouroga <ybouroga@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/02 15:09:59 by ybouroga          #+#    #+#             */
-/*   Updated: 2025/09/07 19:14:03 by ybouroga         ###   ########.fr       */
+/*   Updated: 2025/09/07 19:21:04 by ybouroga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,7 +46,9 @@ int	ft_not_dir_but_file(char *path)
 	is_file = 0;
 	if (ft_strcmp(path, "/") == 0)
 		return (0);
-	if (path && path[0] != 0 && (path[strlen(path) - 1] == '/'))
+	if (path && path[0] != 0 && \
+		(ft_strlen(path) - 1 >= 0) && \
+		(path[ft_strlen(path) - 1] == '/'))
 	{
 		path[strlen(path) - 1] = 0;
 		if (ft_is_file(path))
